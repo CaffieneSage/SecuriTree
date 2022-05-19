fc3d3b23f13d5a957f076b372265a037519838a8
CaffieneSage <jazznaz570@googlemail.com>
2022-05-19 13:02:59 +0100

    Add files via upload
    Added some basic comments for documentation purposes.

--- main.py
+++ main.py
@@ -1,19 +1,29 @@
 import tkinter
 
+# Creates a window element with the tkinter library and sits its size
+# in pixels.
+
 window = tkinter.Tk()
 window.geometry("300x150")
+
+# Creates a label with a prompt to enter username data.
 
 messageUsername = tkinter.Label(text="Please enter you username.")
 messageUsername.pack()
 
+# Creates an entry box to receive username.
+
 userName = tkinter.Entry(window, )
 userName.pack()
+
+# Creates a label with a prompt to enter password.
 
 messagePassword = tkinter.Label(text="Please enter your password.")
 messagePassword.pack()
 
+# Creates an entry box to recieve password input.
+
 userPassword = tkinter.Entry(window, show="*")
 userPassword.pack()
-
 
 window.mainloop()
